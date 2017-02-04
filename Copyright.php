168571137bf241b9345141f1a4697b62776c6779
Wendy May Kwok <wmkwok95@gmail.com>
2017-02-04 12:16:28 -0500

    Added Co[yright information page
    Page 10

---
+++ Copyright.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CopyRight Page</title>
+</head>
+
+<body>
+<p>
+<b>You are free to:</b><br/>
+Share — copy and redistribute the material in any medium or format<br/>
+Adapt — remix, transform, and build upon the material<br/>
+The licensor cannot revoke these freedoms as long as you follow the license terms.<br/>
+</p>
+
+
+<p>
+	<b>Terms</b><br/>
+	Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.<br/>
+	
+	NonCommercial — You may not use the material for commercial purposes.<br/>
+	
+	ShareAlike — If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.<br/>
+</p>
+
+<p>
+	For more information about the copyright please visit <a href="https://creativecommons.org/licenses/by-nc-sa/3.0/">this site</a>.
+</p>
+</body>
+</html>
