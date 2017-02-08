3e03b26c9a077eba53d2ea4ad3aac205fcc52fb8
zhang435 <zhang435@umail.iu.edu>
2017-02-08 09:40:22 -0500

    added search and user profile page

---
+++ SearchResult.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Resourse Result</title>
+</head>
+
+<button type="button">New search</button>
+
+<body>
+    <table style="width:100%">
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Rate</th>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>How to Program in C</td>
+        <td>* * * * *</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Prime C</td>
+        <td>* * *</td>
+      </tr>
+    </table>
+
+</body>
+</html>
---
+++ User_Profile.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<style>
+body {
+    margin: 0;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 25%;
+    background-color: #f1f1f1;
+    position: fixed;
+    height: 100%;
+    overflow: auto;
+}
+
+li a {
+    display: block;
+    color: #000;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+li a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+li a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="#active" href="#home">Overview</a></li>
+  <li><a href="#info">Edit Personal Information</a></li>
+  <li><a href="#resources">Resources</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+  <h2>Content here</h2>
+  <p>Some text..</p>
+  <p>Some text..</p>
+  <p>Some text..</p>
+  <p>Some text..</p>
+  <p>Some text..</p>
+  <p>Some text..</p>
+  <p>Some text..</p>
+</div>
+</body>
+</html>
