425da46e4fcb694942dfc5594657556c89980833
Wendy May Kwok <wmkwok95@gmail.com>
2017-02-10 20:50:32 -0500

    changed some design

--- Login.php
+++ Login.php
@@ -22,17 +22,15 @@
     <div class="form-group">
       
       <label class="mr-sm-2">Username</label>
-      <input type="text" class="form-control" name="userName" value=""/>
+      <input type="text" class="form-control" name="userName" required="required" value=""/>
      
       <label class="mr-sm-2">Password</label>
       <input type="password" class="form-control" minlength="8" maxlength="16" name="password" value=""/>
       </br>
-     <button type="submit">Login</button>
-
+     <button  class="btn btn-secondary" name="submit" type="submit">Login</button>
+	</form>
+	 </div>
     </div>
-
-
-
 <?php include "footer.php"?>
  
 </body>
