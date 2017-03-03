dc5ad59680655568524ea967068fc18e075da9ca
wmkwok <wmkwok@indiana.edu>
2017-03-03 12:58:30 -0500

    added administrators listing

--- Administrator.php
+++ Administrator.php
@@ -12,28 +12,35 @@
     <tr>
       <th width="94" height="71">#</th>
       <th width="246">Name</th>
-      <th width="266">User Name</th>
-      <th width="72"></th>
+      <th width="266">Username</th>
+      <th width="72">Options</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td height="84">1</td>
-      <td>name here</td>
-      <td>username here</td>
-      <td> <img src="fonts/trash.ico" width="25" height="25" /> 
-</td>
 
-    </tr>
-    <tr>
-      <td height="80">2</td>
-      <td>name here</td>
-      <td>username here</td>
-      <td><img src="fonts/trash.ico" width="25" height="25" /> 
-</td>
-    </tr>
-        
-      
+ <?php
+       require_once('connect.php');
+       $conn = connection();
+       mysqli_select_db($conn, "resource");
+       $listAdmins="SELECT name, username FROM users";
+
+       $listAdmins=mysqli_query($conn, $listAdmins);
+       if(mysqli_num_rows($listAdmins)){
+         $counter=1;
+         $options="<td><img src=\"fonts/edit.ico\" width=\"20\" height=\"20\" /><img src=\"fonts/trash.ico\" width=\"25\" height=\"25\" /></td\
+>";
+         while($row=mysqli_fetch_assoc($listAdmins)){
+            echo("<tr>");
+            echo("<td>{$counter}</td>");
+            echo("<td>{$row["name"]}</td>");
+            echo("<td>{$row["username"]}</td>");
+            echo($options);
+            echo("</tr>");
+            $counter+=1;
+         }
+    }
+       mysqli_close($conn);
+       ?>
   </tbody>
 </table>
 
