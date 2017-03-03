e69e76ea7670817a8e9b286e3ae048451f08e051
lincong <lincong@149-160-152-81.dhcp-bl.indiana.edu>
2017-03-02 22:35:21 -0500

    upload sql

--- Resource_upload.php
+++ Resource_upload.php
@@ -16,13 +16,13 @@
       </div>
     </div>
 
-    <div class="container"
-	 <form class="form-inline" method="post" enctype="multipart/form-data">
+    <div class="container">
+	 <form class="form-group" method="post" enctype="multipart/form-data">
 
 	   <p>Please choose a file to upload, then click upload.</p> 
 	   	   
 	   <label class="mr-sm-2">Grade Level</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="gradeLevel" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="gradeLevel" size="1">
 	     <option value="select">Select</option>
 	     <option value="pre">Pre-school</option>
 	     <option value="elementary">Elementary School</option>
@@ -31,7 +31,7 @@
 	   </select></br>
 
 	   <label class="mr-sm-2">Standard</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="standard" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="standard" size="1">
 	     <option value="select">Select</option>
 	     <option value="csta">CSTA 2016 Standards (interim)</option>
 	     <option value="iste">ISTE-Standards for Computer Science Educators</option>
@@ -42,7 +42,7 @@
 
 	 
 	   <label class="mr-sm-2">Resource Type</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="resourceType" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="resourceType" size="1">
 	     <option value="select">Select</option>
 	     <option value="assessment">Assessment</option>
 	     <option value="activity">Activity/Exercise</option>
@@ -51,7 +51,7 @@
 	   </select></br>
 	
 	   <label class="mr-sm-2">Subject</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="subject" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="subject" size="1">
 	     <option value="select">Select</option>
 	     <option value="math">Mathematics</option>
 	     <option value="science">Science</option>
@@ -64,7 +64,7 @@
 	   </select></br>
 
 	   <label class="mr-sm-2">Concept</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="concept" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="concept" size="1">
 	     <option value="select">Select</option>
 	     <option value="algorithms">Algorithms and Programs</option>
 	     <option value="computing">Computing Systems</option>
@@ -76,7 +76,7 @@
 	   
 
 	   <label class="mr-sm-2">Programming language</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="programLanguage" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="programLanguage" size="1">
 	     <option value="select">Select</option>
 	     <option value="scratch">Scratch</option>
 	     <option value="blockly">Blockly</option>
@@ -93,7 +93,7 @@
 	   </select></br>
 
 	   <label class="mr-sm-2">State</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="state" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="state" size="1">
 	     <option value="select">Select</option>
 	     <option value="all">All</option>
 	     <option value="indiana">Indiana</option>
@@ -102,7 +102,7 @@
 	   </select><br>
 
 	   <label class="mr-sm-2">Instructional method</label>
-	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="instructionalMethod" size="1">
+	   <select class="custom-select mb-2 mr-sm-2 mb-sm-0" required name="instructionalMethod" size="1">
 	     <option value="select">Select</option>
 	     <option value="problemBased">Problem-based Learning</option>
 	     <option value="projectBased">Project-Based Learning</option>
@@ -114,73 +114,73 @@
 	     <option value="rolePlay">Role PLay</option>
 	     <option value="assessment">Assessment</option>
 	   </select></br>
-	   
-	   <p>
-	     <label for="searchBar">Keyword Search</label>
-	     <input class="form-control" type="text" name="searchBar" value="" placeholder="ex. Python Exercises"/>
-	
-	   </p>
-
-		<input type="hidden" name="MAX_FILE_SIZE" value="50000" /> 
+	   <br/>
 
 	   <label class="btn btn-success">
-    Choose File <input type="file" name="file" hidden/>
+    	Choose File <input type="file" name="userfile" hidden/>
 		</label><br>
            
-	   <input class="btn btn-secondary" type="submit" name="upload" value="upload"> 
+    <button class="btn btn-secondary" type="submit" name="upload" value="upload"> Upload </button>
 	 </form> 
 </div>
 
+
 <?php 
-        if ( isset( $_POST["upload"] ) ) { 
-         processForm(); 
-        } else { 
-         displayForm(); 
-        } 
 
-        function processForm() { 
-         if ( isset( $_FILES["photo"] ) and $_FILES["photo"]["error"] == UPLOAD_ERR_OK ) { 
-           if ( $_FILES["photo"]["type"] != "image/jpeg" ) { 
-             echo "<p>JPEG photos only, thanks!</p>"; 
-           } elseif ( !move_uploaded_file( $_FILES["photo"]["tmp_name"], "photos/" . basename( $_FILES["photo"]["name"] ) ) ) { 
-             echo "<p>Sorry, there was a problem uploading that file.</p>" . $_FILES["photo"]["error"] ; 
-           } else { 
-             displayThanks(); 
-           } 
-         } else { 
-           switch( $_FILES["photo"]["error"] ) { 
-             case UPLOAD_ERR_INI_SIZE: 
-               $message = "The photo is larger than the server allows."; 
-               break; 
-             case UPLOAD_ERR_FORM_SIZE: 
-               $message = "The photo is larger than the script allows."; 
-               break; 
-             case UPLOAD_ERR_NO_FILE: 
-               $message = "No file was uploaded. Make sure you choose a file to upload."; 
-               break; 
-             default: 
-               $message = "Please contact your server administrator for help."; 
-           } 
-           echo "<p>Sorry, there was a problem uploading that file. $message</p>"; 
-      } 
-     } 
+       if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
+{		
+		$error = $_FILES['userfile']['error'];
+		$fileName = $_FILES['userfile']['name'];
+		echo $fileName;
+		echo '<br/>';
+		
+		$tmpName  = $_FILES['userfile']['tmp_name'];
+		$fileSize = $_FILES['userfile']['size'];
+		$fileType = $_FILES['userfile']['type'];
+	
+		
+		echo $fileSize;
+		echo '<br/>';
+		echo $fileType;
+		echo '<br/>';
+		$gradeLevel = $_POST['gradeLevel'];
+		$standard = $_POST['standard'];
+		$resourceType = $_POST['resourceType'];
+		$subject = $_POST['subject'];
+		$concept = $_POST['concept'];
+		$programLanguage = $_POST['programLanguage'];
+		$state = $_POST['state'];
+		$instructionalMethod = $_POST['instructionalMethod'];
+		$user = $_SESSION['username'];
+		$link = '----';
+		$date = date("j, n, Y");  
+	
+		
 
-     function displayForm() { 
-     ?> 
+		$fp = fopen($tmpName, 'r');
+		$content = fread($fp, $fileSize);
+		$content = addslashes($content);
+		fclose($fp);
+		
+		if(!get_magic_quotes_gpc())
+		{
+			$fileName = addslashes($fileName);
+		}
+		echo("Success upload");
+		include('connect.php');
+		$conn = connection();
+		mysqli_select_db($conn, "files");
 
-     <?php 
-     } 
+		$addFiles = "INSERT INTO `files`(`filename`, `uploadedBy`, `date`, `rating`, `description`, `state`, `gradeLevel`, `link`, `subject`) VALUES ('$fileName','$user','$date', 5, 'good','$state','$gradeLevel','$link','$subject')";
+		
+		mysqli_query($conn, $addFiles) or die(mysqli_error($conn));
+		mysqli_close($conn);
+	
+		echo "<div class=\"alert alert-success\"><strong> '$fileName' Uploaded'</strong></div>";			   
+}		
 
-     function displayThanks() { 
-     ?> 
-        <p>Thanks for uploading your file
-            <?php if ( $_POST["visitorName"] ) echo ", " . $_POST["visitorName"] ?>!
-         </p> 
-        <p>Here's your file:</p> 
-        <p><img src="file/<?php echo $_FILES["file"]["name"] ?>" alt="file"/></p> 
-     <?php 
-     } 
-     ?> 
+
+?> 
 <br/>
 <?php include 'footer.php';?>
 </body>
