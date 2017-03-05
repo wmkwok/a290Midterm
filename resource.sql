fa618359945a68dae602b5f6186072cf0a4fc095
zhang435 <zhang435@umail.iu.edu>
2017-03-04 23:54:17 -0500

    DONE

--- resource.sql
+++ resource.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.2
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 04, 2017 at 04:52 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Mar 05, 2017 at 05:53 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -43,8 +43,7 @@ CREATE TABLE `files` (
 --
 
 INSERT INTO `files` (`filename`, `uploadedBy`, `date`, `rating`, `description`, `state`, `gradeLevel`, `link`, `subject`) VALUES
-('Assignment1.docx', 'testing', '3, 3, 2017', 5, 'good', 'indiana', 'elementary', '----', 'languageArts'),
-('test.pdf', 'wmkwok', 'today', 5, 'wendy\'s test file', 'Indiana', 'College', '#', 'Math');
+('temp.rtf', 'applezhang', '5, 3, 2017', 5, 'good', 'indiana', 'middle', '----', 'science');
 
 -- --------------------------------------------------------
 
@@ -56,14 +55,6 @@ CREATE TABLE `tags` (
   `name` varchar(20) NOT NULL,
   `filename` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `tags`
---
-
-INSERT INTO `tags` (`name`, `filename`) VALUES
-('math', 'test.pdf'),
-('python', 'test.pdf');
 
 -- --------------------------------------------------------
 
@@ -88,9 +79,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `name`, `email`, `password`, `schooltype`, `schoollevel`, `state`, `bio`, `admin`) VALUES
-('testing', 'test', 'testing@gmail.com', '$2y$10$bHnDKRdmB2Mrpf4adXabl.69MM7fg40gDSLN9RN3hytrntGV5rXdu', 'public', 'elementary', 'Indiana', 'dsfsfsdfs', 1),
-('wendyk', 'wendy', 'wendyk@gmail.com', '$2y$10$OIieW02tABwpntznsAZ/HeMDHXPIiSzgii/p3dSOxBKtOVSILg8Na', 'other', 'college', 'indiana', 'I\'m wendy.', 0),
-('wmkwok', 'wendy kwok', 'wmkwok95@gmail.com', '$2y$10$3wN3WvzqjsBiQdnR4FwfteJlrbvqLFBBKojIooWUVUY5QzYet..4W', 'other', 'college', 'indiana', '', 0);
+('admin2', 'admin2', '1234567890@q.com', '$2y$10$a21sierNxr7TRnrznKlR9eJm8e3BjbzJurWLCiTN8sdjr4Y0NUmJ.', 'private', 'elementary', 'Louisiana', 'ds', 1),
+('applezhang', 'iijoijoi', 'apple19950105@qq.com', '$2y$10$9JQWhOI.Ugrno9hmrYTA8OUJLh8g2imMhkC.i4r.DheeLoUKzVSei', 'charter', 'high', 'Arkansas', 'hjh', 0),
+('ksdjfbksdfbk', 'sdkfjksdfjb', 'ksdj@qq.com', '$2y$10$3874QTxgjlbhYBqBWUqju.YZZGJnsp5nPTrRXz5AEI8TDGhlgiNPC', 'charter', 'preschool', 'Kentucky', 'xcv', 0),
+('sdkfsdlkfb', 'sdhfb', 'sdfkb@q.om', '$2y$10$aVmP5G3e3.9BgVHa3XRJTeOZrVaavhM59Q7AMTTlVwfBIAYt.TFAC', 'private', 'elementary', 'Indiana', '', 0),
+('thisisadmin', 'admin', 'asd@qq.com', '$2y$10$FyUlbiLZ6QxUcFBUJ3FwZujtpdbS.hMJkTzxzVMjBbLLH63dsSnb2', 'private', 'middle', 'Louisiana', '', 0),
+('zhang435', 'test', 'testing@gmai2.com', '$2y$10$9JQWhOI.Ugrno9hmrYTA8OUJLh8g2imMhkC.i4r.DheeLoUKzVSei', 'public', 'elementary', 'Indiana', 'dsfsfsdfs', 1);
 
 --
 -- Indexes for dumped tables
